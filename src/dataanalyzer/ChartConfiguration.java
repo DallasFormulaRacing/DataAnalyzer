0cab86e608ede9c096f353a2c013c61acaeb67a6
nolanrdavenport <54985953+nolanrdavenport@users.noreply.github.com>
2019-09-29 17:45:33 -0500

    ChartConfiguration
    Cleaned up the ChartConfiguration code

--- src/dataanalyzer/ChartConfiguration.java
+++ src/dataanalyzer/ChartConfiguration.java
@@ -1,26 +1,25 @@
-/*
- * This file will hold an array of chart data
- */
 package dataanalyzer;
 import java.util.ArrayList;
 import javax.swing.*;
+
 /**
- *
+ * This class will hold an array of chart data.
+ * 
  * @author Nolan Davenport
  */
 public class ChartConfiguration {
     private int numCharts;
     private ArrayList<ChartAssembly> charts;
    
-    //Holds the data for each chart in the configuration
+    //Holds the data for each chart in the configuration.
     class ChartLocation{
         public String fileName;
         
-        //These are proportions of the length of the main data analyzer length. These should be >0 and <1
+        //These are proportions of the dimention and location of the main data analyzer length. These should be >0 and <1.
         public float x,y;
         public float width,height;
         
-        //Constructs an instance of ChartLocation
+        //Constructs an instance of ChartLocation.
         public ChartLocation(String fileName, float x, float y, float width, float height){
             this.fileName = fileName;
             this.x = x;
@@ -31,28 +30,39 @@ public class ChartConfiguration {
     };
     private ArrayList<ChartLocation> locations = new ArrayList<ChartLocation>();
     
-    //for creating a new chart configuration
+    /**
+     * Creates a new chart configuration based on the current state of the charts on screen.
+     * @param charts ArrayList of charts that are going to be saved in a chart configuration.
+     * @param dataAnalyzer The instantiated object of the DataAnalyzer class. Used to get the dimensions of the window. 
+     */
     public ChartConfiguration(ArrayList<ChartAssembly> charts, DataAnalyzer dataAnalyzer){
         this.charts = charts;
         numCharts = charts.size();
         for(ChartAssembly chart : charts){
             JInternalFrame chartFrame = chart.getChartFrame();
             
-            float x = chartFrame.getX() / dataAnalyzer.getX();
-            float y = chartFrame.getY() / dataAnalyzer.getY();
-            float width = chartFrame.getWidth() / dataAnalyzer.getWidth();
-            float height = chartFrame.getHeight() / dataAnalyzer.getHeight();
+            float x = (float)chartFrame.getX() / dataAnalyzer.getWidth();
+            float y = (float)chartFrame.getY() / dataAnalyzer.getHeight();
+            float width = (float)chartFrame.getWidth() / dataAnalyzer.getWidth();
+            float height = (float)chartFrame.getHeight() / dataAnalyzer.getHeight();
             
-            locations.add(new ChartLocation("", x, y, width, height));//TODO: figure out how to get file name of a chart
+            locations.add(new ChartLocation("", x, y, width, height));//TODO: figure out how to get file name of a chart.
         }
     }
     
-    //For opening an existic chart configuration
+    /**
+     * Opens an existing chart configuration.
+     * @param filename File name of the saved chart configuration.
+     */
     public ChartConfiguration(String filename){//The file name should be a .dfrchartconfig
         //TODO: Talk with people making an installation process to figure out file directories. 
     }
     
-    public void saveChartConfiguration(){
+    /**
+     * 
+     * @param filename filename of the file to be saved. Chosen by the user during the save process. 
+     */
+    public void saveChartConfiguration(String filename){
         //TODO: Figure out format
         
         //Possible format for each location per line
