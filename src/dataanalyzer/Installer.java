72f0d27f4ee3aff30035d3d8be6685f3416eef21
Arib Dhuka <atxarib99@gmail.com>
2019-10-12 12:12:06 -0500

    Setup structure for installer code sequences to run

---
+++ src/dataanalyzer/Installer.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataanalyzer;
+
+/**
+ *
+ * @author aribdhuka
+ */
+public class Installer {
+    
+    public static void runInstaller() {
+        //check os
+        
+        //if Windows
+            runWindowsInstaller();
+        //else if Linux
+            runLinuxInstaller();
+    }
+    
+    private static void runWindowsInstaller() {
+        //TODO: Shadman's code
+    }
+    
+    private static void runLinuxInstaller() {
+        //TODO: Angela's code
+    }
+    
+}
